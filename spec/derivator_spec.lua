77eb98105f0e1b831ac51f8888097b34ba02c69d
kikito <kikito@gmail.com>
2013-08-19 13:28:46 +0200

    retab

--- spec/derivator_spec.lua
+++ spec/derivator_spec.lua
@@ -1,11 +1,6 @@
--- describe("hola", function()
---             it("trues", function()
---                   assert.is_truthy(1)
---             end)
--- end)
-
 local derivator = require 'derivator'
+
 describe('adding', function()
-               it('adds paths', function()
-                                end)
-                      end)
+  it('adds paths', function()
+  end)
+end)
