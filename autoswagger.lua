346b1148932378cde0455188eacc4eec62cb0ad8
kikito <kikito@gmail.com>
2013-08-26 13:23:44 +0200

    included api.lua in autoswagger.lua

--- autoswagger.lua
+++ autoswagger.lua
@@ -3,11 +3,13 @@ local PATH = (...):match("(.+%.)[^%.]+$") or ""
 local base   = require(PATH .. 'autoswagger.base')
 local Parser = require(PATH .. 'autoswagger.parser')
 local Host   = require(PATH .. 'autoswagger.host')
+local API    = require(PATH .. 'autoswagger.api')
 
 return {
   EOL    = base.EOL,
   Parser = Parser,
-  Host   = Host
+  Host   = Host,
+  API    = API
 }
 
 
