3043af727f34657447ef514daa4814127c23833b
kikito <kikito@gmail.com>
2013-08-28 10:53:39 +0200

    renamed Parser to Brain

--- autoswagger.lua
+++ autoswagger.lua
@@ -1,14 +1,14 @@
 local PATH = (...):match("(.+%.)[^%.]+$") or ""
 
 local base      = require(PATH .. 'autoswagger.base')
-local Parser    = require(PATH .. 'autoswagger.parser')
+local Brain     = require(PATH .. 'autoswagger.brain')
 local Host      = require(PATH .. 'autoswagger.host')
 local API       = require(PATH .. 'autoswagger.api')
 local Parameter = require(PATH .. 'autoswagger.parameter')
 
 return {
   EOL     = base.EOL,
-  Parser  = Parser,
+  Brain  = Brain,
   Host    = Host,
   API     = API,
   Param   = Parameter
--- autoswagger/brain.lua
+++ autoswagger/brain.lua
@@ -4,31 +4,31 @@ local base   = require(PATH .. '.base')
 local array  = require(PATH .. '.array')
 local Host   = require(PATH .. '.Host')
 
-local Parser = {}
+local Brain = {}
 
-function Parser:new(threshold, unmergeable_tokens)
+function Brain:new(threshold, unmergeable_tokens)
   return setmetatable({
     threshold          = threshold          or 1.0,
     unmergeable_tokens = unmergeable_tokens or {},
     hosts = {}
   }, {
-    __index = Parser
+    __index = Brain
   })
 end
 
-function Parser:newHost(hostname)
+function Brain:newHost(hostname)
   self.hosts[hostname] = self.hosts[hostname] or
     Host:new(hostname, self.threshold, self.unmergeable_tokens)
   return self.hosts[hostname]
 end
 
-function Parser:get_paths(hostname)
+function Brain:get_paths(hostname)
   local host = self.hosts[hostname]
   return host and host:get_paths() or {}
 end
 
-function Parser:learn(method, hostname, path, query, body, headers)
+function Brain:learn(method, hostname, path, query, body, headers)
   self:newHost(hostname):learn(method, path)
 end
 
-return Parser
+return Brain
